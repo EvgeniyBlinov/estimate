2a599d6ef7eabee5c42a8e72f5fa62384b90ed54
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2021-09-20 22:09:35 +0300

    master
    Work stage 2021-09-20 22:09:35
     M estimate/__main__.py

--- estimate/__main__.py
+++ estimate/__main__.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-from .command_line import CliRunner
+from estimate import CliRunner
 
 if __name__ == "__main__":
     cli = CliRunner()
